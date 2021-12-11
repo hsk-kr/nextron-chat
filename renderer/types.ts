9156170708f867a3f47dd3cbfefd95a27ed283c5
hsk.coder@gmail.com <hsk.coder@gmail.com>
2021-12-12 08:32:32 +0900

    Update types

--- renderer/types.ts
+++ renderer/types.ts
@@ -12,6 +12,26 @@ export interface IAlertContext {
   showSuccessMsg: (msg: string) => void;
 }
 
+export interface Message {
+  chatRoomId: string;
+  message: string;
+  sender: string;
+  sentAt: number;
+  mine?: boolean;
+  senderEmail?: string;
+}
+
+export interface ChatRoom {
+  id?: string;
+  type: 'group' | 'private';
+  members: string[];
+}
+
+export type SelectedChatRoom =
+  | ChatRoom
+  | undefined
+  | { to: { email: string; uid: string } };
+
 export type AlertType = 'error' | 'warning' | 'info' | 'success';
 
 export type AlertState = {
