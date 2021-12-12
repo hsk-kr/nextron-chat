b25e39d83c136fc1ce1af9183ffb884f2091498d
hsk.coder@gmail.com <hsk.coder@gmail.com>
2021-12-12 19:57:50 +0900

    Fix sendMessage and Add leaveChatRoom Functions
    @ sendMessage
    
    - Changed result status codes
    - Added Permission check logic
    - Check a type of a chat room then add chatRoomIds to users
     (Only private chat can be created by sending messages)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,8 @@ const FUNCTIONS_REGION = 'asia-east1';
 const cors = require('cors')({ origin: true });
 const admin = require('firebase-admin');
 admin.initializeApp();
-const db = admin.firestore();
+const firestore = admin.firestore;
+const db = firestore();
 
 export const createUserDocument = functions
   .region(FUNCTIONS_REGION)
@@ -71,12 +72,12 @@ export const sendMessage = functions
         const decodedToken = await admin.auth().verifyIdToken(token);
         uid = decodedToken.uid;
       } catch {
-        return res.status(500).json({
+        return res.status(401).json({
           message: 'Failed to verify the token',
         });
       }
 
-      if (!chatRoomId) {
+      if (chatRoomId === undefined) {
         // It has to create a chat room
         chatRoomId = makeChatRoomId(uid, to);
 
@@ -89,10 +90,35 @@ export const sendMessage = functions
               members: [uid, to],
             });
         }
-      }
 
-      appendChatRoomIfUserDoesNotHave(uid, chatRoomId);
-      appendChatRoomIfUserDoesNotHave(to, chatRoomId);
+        appendChatRoomIfUserDoesNotHave(uid, chatRoomId);
+        appendChatRoomIfUserDoesNotHave(to, chatRoomId);
+      } else {
+        const chatRoom = await db.collection('chatRooms').doc(chatRoomId).get();
+
+        if (!chatRoom.exists) {
+          return res.status(404).json({
+            message: "The chatroom doesn't exist",
+          });
+        }
+
+        const { type, members } = chatRoom.data();
+
+        // If the sender aren't in the members of the chat
+        if (!members.includes(uid)) {
+          return res.status(401).json({
+            message: "You don't have a permission",
+          });
+        }
+
+        // Reuse private chatRoom
+        if (type === 'private') {
+          const to = members.filter((member: string) => member !== uid)[0];
+
+          appendChatRoomIfUserDoesNotHave(uid, chatRoomId);
+          appendChatRoomIfUserDoesNotHave(to, chatRoomId);
+        }
+      }
 
       await db.collection('messages').add({
         chatRoomId,
@@ -103,6 +129,77 @@ export const sendMessage = functions
 
       return res.status(200).json({
         id: chatRoomId,
+        message: 'OK',
+      });
+    });
+  });
+
+export const leaveChatRoom = functions
+  .region(FUNCTIONS_REGION)
+  .https.onRequest((req, res) => {
+    const removeMemberInChatRoomIfChatIsGroup = async (
+      uid: string,
+      chatRoomId: string
+    ) => {
+      const doc = await db.collection('chatRooms').doc(chatRoomId).get();
+
+      if (!doc.exists) return;
+
+      const data = doc.data();
+      if (data.type === 'private') return;
+
+      let members = data.members;
+      members = members.filter((userId: any) => userId !== uid);
+
+      db.collection('chatRooms').doc(chatRoomId).update({
+        members,
+      });
+    };
+
+    const removeChatRoomInUserChats = async (
+      uid: string,
+      chatRoomId: string
+    ) => {
+      const doc = await db.collection('users').doc(uid).get();
+
+      if (!doc.exists) return;
+
+      let chats = doc.data().chats;
+      chats = chats.filter((chatId: any) => chatRoomId !== chatId);
+
+      db.collection('users').doc(uid).update({
+        chats,
+      });
+    };
+
+    return cors(req, res, async () => {
+      if (req.method !== 'POST') {
+        return res.status(401).json({
+          message: 'Not allowed',
+        });
+      }
+
+      const { chatRoomId, token }: any = req.body;
+      if (!chatRoomId) {
+        return res.status(400).json({
+          message: 'Invalid Parameters',
+        });
+      }
+
+      let uid: string = '';
+      try {
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        uid = decodedToken.uid;
+      } catch {
+        return res.status(401).json({
+          message: 'Failed to verify the token',
+        });
+      }
+
+      removeMemberInChatRoomIfChatIsGroup(uid, chatRoomId);
+      removeChatRoomInUserChats(uid, chatRoomId);
+
+      return res.status(200).json({
         message: 'OK',
       });
     });
